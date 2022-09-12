4abd735a989e900e699ac1a7b3fa8bbc5a7aeabb
Vishwajeet <67817041+Vishwajeet1709@users.noreply.github.com>
2022-09-12 13:35:12 +0530

    Create 01.c

--- 01.c
+++ 01.c
@@ -7,7 +7,7 @@ int main()
     scanf("%d",&n);
 
     if(n==0)
-        printf("Please try again, it is neither NEAGITVE nor POSITIVE.");
+        printf("Please try again, it is neither NEAGITVE nor POSITIVE.\n");
     else if(n>0)
         printf("You have entered a POSITIVE number.");
     else
