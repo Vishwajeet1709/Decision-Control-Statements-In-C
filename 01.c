80c6c4619332f5a4745a376cfeb38eb032f687b7
Vishwajeet <67817041+Vishwajeet1709@users.noreply.github.com>
2022-09-12 13:00:25 +0530

    Add files via upload

---
+++ 01.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+
+    if(n==0)
+        printf("Please try again, it is neither NEAGITVE nor POSITIVE.");
+    else if(n>0)
+        printf("You have entered a POSITIVE number.");
+    else
+        printf("You have entered a NEGATIVE number.");
+    return 0;
+}
