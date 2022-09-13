c6b2cbb501749b366db49c6c2840755cca6303ce
Vishwajeet <67817041+Vishwajeet1709@users.noreply.github.com>
2022-09-13 10:03:19 +0530

    Add files via upload

---
+++ 02.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+
+    if(n%10==0 || n%10==5)
+        printf("Yes, %d is divisible by 5.",n);
+    else
+        printf("No, %d is not divisible by 5.",n);
+    return 0;
+}
