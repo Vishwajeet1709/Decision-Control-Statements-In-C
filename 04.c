4b2e5a565f42e551305d6bca52b781f7a0a0680e
Vishwajeet <67817041+Vishwajeet1709@users.noreply.github.com>
2022-09-16 12:06:11 +0530

    Add files via upload

---
+++ 04.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    //check even or odd without % Operator
+    printf("Enter a number : ");
+    scanf("%d",&n);
+
+    if(n&1==0)
+        printf("%d is a Even number.",n);
+    else
+        printf("%d is a Odd number.",n);
+    return 0;
+}
