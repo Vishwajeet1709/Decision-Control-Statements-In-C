f9a5f23e7480f19e77dea18e050b079957c16b9a
Vishwajeet <67817041+Vishwajeet1709@users.noreply.github.com>
2022-09-26 13:24:58 +0530

    Add files via upload

--- 05.c
+++ 05.c
@@ -1,1 +1,16 @@
 
+#include<stdio.h>
+int main()
+{
+    int n;
+    //check whether the number is three digit or not
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    if(n/10==0)
+        printf("%d is not a three digit number",n);
+    else if(n/100==0)
+        printf("%d is not a three digit number",n);
+    else
+        printf("%d is a three digit number",n);
+    return 0;
+}
