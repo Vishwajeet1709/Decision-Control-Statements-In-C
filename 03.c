cbf0a9f517167f7fec6905a578060bb705f6c727
Vishwajeet <67817041+Vishwajeet1709@users.noreply.github.com>
2022-09-28 21:23:20 +0530

    Update 03.c

--- 03.c
+++ 03.c
@@ -3,7 +3,7 @@ int main()
 {
     int n;
     // program to check numbers are even or odd
-    printf("Enter a number :");
+    printf("Enter a number : ");
     scanf("%d",&n);
     if(n%2==0)
         printf("%d is a Even number.\n\n",n);
