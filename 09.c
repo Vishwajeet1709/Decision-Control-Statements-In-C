ead2a169132c9994aa575c49b4188690f6067f0c
Vishwajeet <67817041+Vishwajeet1709@users.noreply.github.com>
2022-10-02 19:21:55 +0530

    Update 09.c

--- 09.c
+++ 09.c
@@ -6,12 +6,12 @@ int main()
     printf("Enter any three numbers to check which is greater :");
     scanf("%d%d%d",&a,&b,&c);
     if(a==b && b==c)
-        printf("All the numbers you have entered are same.");
+        printf("All the numbers you have entered are same.\n\n");
     else if(a>=b && a>=c)
-        printf("%d is the greatest number.",a);
+        printf("%d is the greatest number. \n\n",a);
     else if(b>=c)
-        printf("%d is the greatest number.",b);
+        printf("%d is the greatest number. \n\n",b);
     else
-        printf("%d is greatest number.",c);
+        printf("%d is greatest number. \n\n",c);
     return 0;
 }
